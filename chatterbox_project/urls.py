2cacecdfdecab13b37fc1db9c37887310c87621b
PetrA <petr.adamek.kt@post.cz>
2022-09-06 20:50:24 +0200

    Pridano view hello

--- chatterbox_project/urls.py
+++ chatterbox_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import chatterbox.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', chatterbox.views.hello),
+    #path('/', chatterbox.urls),
 ]
