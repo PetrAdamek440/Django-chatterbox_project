7e6edcf7e9581c2829eded8d843d1f83a064c5f7
PetrA <petr.adamek.kt@post.cz>
2022-09-14 18:16:18 +0200

    Version by Petr Gregor from 2022-09-12-1

--- chatterbox/views.py
+++ chatterbox/views.py
@@ -1,16 +1,33 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from chatterbox.models import Room, Message
 
+
 # Create your views here.
 def hello(request, s):
-    return HttpResponse(f'Hello {s} World!!!')
+    return HttpResponse(f'Hello, {s} world!')
+
+
+def home(request):
+    rooms = Room.objects.all()  # najdeme všechny místnosti
+
+    context = {'rooms': rooms}
+    return render(request, 'chatterbox/home.html', context)
+
 
 def search(request, s):
     rooms = Room.objects.filter(name__contains=s)
-
     messages = Message.objects.filter(body__contains=s)
 
     context = {'rooms': rooms, 'messages': messages}
-    return render(request, 'chatterbox/search.html', context)
+    return render(request, "chatterbox/search.html", context)
+
+
+def room(request, pk):
+    room = Room.objects.get(id=pk)  # najdeme místnost se zadaným id
+    messages = Message.objects.filter(room=pk)  # vybereme všechny zprávy dané místnosti
+
+    context = {'room': room, 'messages': messages}
+    return render(request, "chatterbox/room.html", context)
+
--- chatterbox_project/settings.py
+++ chatterbox_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1!%fe+50rq#gc(rl)xvakdhyn&fyz_l(90&p2kwetm22=zm39#'
+SECRET_KEY = 'django-insecure-5g8axmj%(#wy(r@3o1r1w6mtf7r(r-#b8kg@21k2%cf9so4pkc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
